c03c90625eb4ff0f81f02068f1d2ac78107d3f1b
Scars and Bullets <sbrough@gmail.com>
2019-05-29 21:29:19 -0600

    updatest

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,21 +3,12 @@ var db = require("../models");
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
-    db.Hunt.findAll({}).then(function (dbHunt) {
-      res.render("index", {
-        msg: "Welcome!",
-        hunts: dbHunt
-      });
-    });
+    res.render("index");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/hunt/:id", function (req, res) {
-    db.Hunt.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.render("hunt", {
-        hunt: dbHunt
-      });
-    });
+  // Load create hunt page
+  app.get("/createHunt/", function (req, res) {
+    res.render("createhunt");
   });
 
   // Render 404 page for any unmatched routes
@@ -25,31 +16,3 @@ module.exports = function (app) {
     res.render("404");
   });
 };
-
-// var db = require("../models");
-
-// module.exports = function(app) {
-//   // Load index page
-//   app.get("/", function(req, res) {
-//     db.Example.findAll({}).then(function(dbExamples) {
-//       res.render("index", {
-//         msg: "Welcome!",
-//         examples: dbExamples
-//       });
-//     });
-//   });
-
-//   // Load example page and pass in an example by id
-//   app.get("/example/:id", function(req, res) {
-//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.render("example", {
-//         example: dbExample
-//       });
-//     });
-//   });
-
-//   // Render 404 page for any unmatched routes
-//   app.get("*", function(req, res) {
-//     res.render("404");
-//   });
-// };
