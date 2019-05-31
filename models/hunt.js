d3b6d56af84d0c973425401d79b3f53dbd2a9878
Scars and Bullets <sbrough@gmail.com>
2019-05-30 19:00:12 -0600

    updated models

--- models/hunt.js
+++ models/hunt.js
@@ -6,6 +6,13 @@ module.exports = function (sequelize, DataTypes) {
       validate: {
         len: [1]
       }
+    },
+    huntCode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
     }
   });
 
--- models/question.js
+++ models/question.js
@@ -1,12 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
   var Question = sequelize.define("Question", {
-    question: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
     clue: {
       type: DataTypes.STRING,
       allowNull: false,
